3353a249c39abe2f51b0b0641c3bac0be6ce5d4e
saimaanav <saimaanav@gmail.com>
2025-03-02 21:46:33 -0500

    Update WinProbabilityModel.py

--- WinProbabilityModel.py
+++ WinProbabilityModel.py
@@ -7,7 +7,7 @@ def predict_win_probability(Sk_A, Sk_B, beta=1):
     skill_diff = Sk_A - Sk_B  # Define x as the skill difference
     return logistic(skill_diff, beta)
 
-# Example Inputs
+# Skill-factor Inputs
 Sk_A =  19.441158001442925 # Team A's skill
 Sk_B = 18.768183098883284 # Team B's skill
 beta = 0.2592  # Weight of skill difference
